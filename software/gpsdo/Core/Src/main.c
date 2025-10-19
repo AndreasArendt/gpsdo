170429f809f5a9c832b3ec7aa920949747070d92
aarendt <andi.arendt@icloud.com>
2025-10-19 17:12:59 +0200

    add missing include

--- software/gpsdo/Core/Src/main.c
+++ software/gpsdo/Core/Src/main.c
@@ -24,6 +24,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "pps.h"
+#include "usb.h"
 
 /* USER CODE END Includes */
 
